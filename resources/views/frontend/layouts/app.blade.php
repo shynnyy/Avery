50e62d9517ded96464e0370a22fad4fa5bb367a6
Shynnyy <shynnyshaquille@gmail.com>
2024-04-02 18:23:59 +0700

    nav, footer, header fix

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>login</title>
-  <link rel="stylesheet" href="{{ asset('frontend/style.css')}}">
-</head>
-<body>
-    @include('frontend.layouts.navbar')
-    <div class="content">
-        @yield('contents')
-    </div>
-    @include('frontend.layouts.footer')
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('frontend/css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        @include('frontend.layouts.navbar')
+        <!-- Header-->
+        @include('frontend.layouts.header')
+
+        <!-- Section-->
+        <div class="content">
+            @yield('contents')
+        </div>
+
+        <!-- Footer-->
+        @include('frontend.layouts.footer')
+
+        <!-- Bootstrap core JS-->
+        <script src="{{ asset('frontend/vendor/bootstrap5/bootstrap.bundle.min.js')}}"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset('frontend/js/script.js')}}"></script>
+    </body>
 </html>
